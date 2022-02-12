d9c0a8a81a550ad6e492a8d59df911a31d4593bb
Nathan Marley <nathan.marley@gmail.com>
2022-02-12 07:20:20 -0300

    Fix test util to check all in 1 run

--- test_util/main.go
+++ test_util/main.go
@@ -3,9 +3,6 @@ package main
 // Test utility to check each exchange API one-by-one. It can help debug
 // exchange API routes which are no longer working and should be removed, or
 // updated.
-//
-// TODO: Don't panic() below, instead print a good/bad message so all exchanges
-// can be checked in just 1 run.
 
 import (
 	"fmt"
@@ -15,60 +12,46 @@ import (
 )
 
 func main() {
-	// 1. Fetch BTC/USD rate
-	coinCapAPI := dashrates.NewCoinCapAPI()
-	coinCapRate, err := coinCapAPI.FetchRate()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("coinCapRate: %+v\n", coinCapRate)
-
-	// now we have BTC/USD rate
-	rateBitcoinUSD := coinCapRate.LastPrice
-	fmt.Printf("rateBitcoinUSD: %+v\n", rateBitcoinUSD)
-
-	// 2. For each exchange, pull the rate
+	// For each exchange rate API, try to pull the rate
 	apis := []dashrates.RateAPI{
+		dashrates.NewBiboxAPI(),
+		dashrates.NewBigONEAPI(),
+		dashrates.NewBinanceAPI(),
+		dashrates.NewBitbnsAPI(),
+		dashrates.NewBitfinexAPI(),
+		dashrates.NewBittrexAPI(),
+		dashrates.NewBvnexAPI(),
+		dashrates.NewCexAPI(),
+		dashrates.NewCoinCapAPI(),
+		dashrates.NewCoinbaseAPI(),
+		dashrates.NewCoinbaseProAPI(),
 		dashrates.NewCrex24API(),
 		dashrates.NewDigifinexAPI(),
-		dashrates.NewCexAPI(),
-		dashrates.NewBinanceAPI(),
-		dashrates.NewBvnexAPI(),
-		dashrates.NewUpholdAPI(),
-		dashrates.NewKuCoinAPI(),
-		dashrates.NewCoinbaseProAPI(),
-		dashrates.NewBittrexAPI(),
-		dashrates.NewBigONEAPI(),
-		dashrates.NewCoinCapAPI(),
-		dashrates.NewTrivAPI(),
-		dashrates.NewIndodaxAPI(),
-		dashrates.NewWhiteBITAPI(),
-		dashrates.NewLiquidAPI(),
-		dashrates.NewSouthXchangeAPI(),
-		dashrates.NewBitfinexAPI(),
-		dashrates.NewHuobiAPI(),
-		dashrates.NewPoloniexAPI(),
-		dashrates.NewBiboxAPI(),
 		dashrates.NewExmoAPI(),
-		dashrates.NewCoinbaseAPI(),
 		dashrates.NewHitBTCAPI(),
-		dashrates.NewOKExAPI(),
-		dashrates.NewBitbnsAPI(),
+		dashrates.NewHuobiAPI(),
+		dashrates.NewIndodaxAPI(),
 		dashrates.NewKrakenAPI(),
+		dashrates.NewKuCoinAPI(),
+		dashrates.NewLiquidAPI(),
+		dashrates.NewOKExAPI(),
+		dashrates.NewPoloniexAPI(),
+		dashrates.NewSouthXchangeAPI(),
+		dashrates.NewTrivAPI(),
+		dashrates.NewUpholdAPI(),
+		dashrates.NewWhiteBITAPI(),
 		dashrates.NewYobitAPI(),
 	}
 
 	for _, api := range apis {
-		rate, err := api.FetchRate()
-		// TODO: Don't panic() here, print a good or bad message so all
-		// exchanges can be checked in just 1 run.
+		_, err := api.FetchRate()
 		if err != nil {
-			// maybe print this err message to stderr and a "<exch> BAD"
-			// message to stdout
+			// print err message to stderr
 			fmt.Fprintf(os.Stderr, "error: %v\n", err.Error())
-			panic(err)
+			// print a <exch> BAD message to stdout
+			fmt.Fprintf(os.Stdout, "%v ERROR\n", api.DisplayName())
+		} else {
+			fmt.Fprintf(os.Stdout, "%v OK\n", api.DisplayName())
 		}
-		fmt.Printf("exch: %v, rate: %+v\n", api.DisplayName(), rate)
-		// usdRate, err := getDashRateInUSD(rateBitcoinUSD, api.DisplayName(), rate)
 	}
 }
